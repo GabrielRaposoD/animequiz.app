4998b89fba0bc5b2d0e23b03214ab8ca6e3f611c
Gabriel Raposo <gabriel.robertor@gmail.com>
2023-10-13 08:55:10 -0700

    removes rate limit

--- src/app/api/animes/route.ts
+++ src/app/api/animes/route.ts
@@ -1,32 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { Ratelimit } from '@upstash/ratelimit';
-import { Redis } from '@upstash/redis';
 import { prisma } from '@/lib';
 
-const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(20, '10 s'),
-});
-
 export async function GET(request: NextRequest) {
-  const ip = request.ip ?? '127.0.0.1';
-  const { limit, reset, remaining } = await ratelimit.limit(ip);
-
-  if (remaining < 1) {
-    return NextResponse.json(
-      { error: 'Rate limit exceeded' },
-      {
-        status: 429,
-        headers: {
-          'X-RateLimit-Limit': limit.toString(),
-          'X-RateLimit-Remaining': remaining.toString(),
-          'X-RateLimit-Reset': reset.toString(),
-        },
-      }
-    );
-  }
-
   const searchParams = request.nextUrl.searchParams;
 
   const query = searchParams.get('title');
@@ -58,11 +34,5 @@ export async function GET(request: NextRequest) {
     take: 100,
   });
 
-  return NextResponse.json(animes, {
-    headers: {
-      'X-RateLimit-Limit': limit.toString(),
-      'X-RateLimit-Remaining': remaining.toString(),
-      'X-RateLimit-Reset': reset.toString(),
-    },
-  });
+  return NextResponse.json(animes);
 }
--- src/app/api/game/success/route.ts
+++ src/app/api/game/success/route.ts
@@ -1,33 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 import { Prisma } from '@prisma/client';
-import { Ratelimit } from '@upstash/ratelimit';
-import { Redis } from '@upstash/redis';
 import { prisma } from '@/lib';
 
-const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(2, '10 s'),
-});
-
 export async function POST(request: NextRequest) {
-  const ip = request.ip ?? '127.0.0.1';
-  const { limit, reset, remaining } = await ratelimit.limit(ip);
-
-  if (remaining < 1) {
-    return NextResponse.json(
-      { error: 'Rate limit exceeded' },
-      {
-        status: 429,
-        headers: {
-          'X-RateLimit-Limit': limit.toString(),
-          'X-RateLimit-Remaining': remaining.toString(),
-          'X-RateLimit-Reset': reset.toString(),
-        },
-      }
-    );
-  }
-
   const res = await request.json();
 
   const seed = await prisma.seed.findUnique({
@@ -58,11 +34,5 @@ export async function POST(request: NextRequest) {
     },
   });
 
-  return NextResponse.json('success', {
-    headers: {
-      'X-RateLimit-Limit': limit.toString(),
-      'X-RateLimit-Remaining': remaining.toString(),
-      'X-RateLimit-Reset': reset.toString(),
-    },
-  });
+  return NextResponse.json('success');
 }
